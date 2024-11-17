02518b6911c02d8f792e4802d01e7d340bb30b0c
khalidwaheli <oukhakhalid@gmail.com>
2024-11-17 15:15:58 +0100

    As a developer, I want to create a version-controlled Git repository for the project

---
+++ src/main/java/com/citronix/api/CitronixFarmApiApplication.java
@@ -0,0 +1,13 @@
+package com.citronix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitronixFarmApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitronixFarmApiApplication.class, args);
+    }
+
+}
