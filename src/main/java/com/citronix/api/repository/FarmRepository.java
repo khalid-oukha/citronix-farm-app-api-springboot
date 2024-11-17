ed8ae8935e967d26ce262422c6bf760111a3e1f2
khalidwaheli <oukhakhalid@gmail.com>
2024-11-17 19:56:48 +0100

    added repository layer for farm

---
+++ src/main/java/com/citronix/api/repository/FarmRepository.java
@@ -0,0 +1,7 @@
+package com.citronix.api.repository;
+
+import com.citronix.api.domain.Farm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FarmRepository extends JpaRepository<Farm,Long> {
+}
