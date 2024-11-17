1683eb3de5725708857d2be39aff426015cd4e54
khalidwaheli <oukhakhalid@gmail.com>
2024-11-17 17:04:52 +0100

    define the field entity

---
+++ src/main/java/com/citronix/api/domain/Field.java
@@ -0,0 +1,31 @@
+package com.citronix.api.domain;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Field {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private double area;
+
+    @ManyToOne
+    @JoinColumn(name = "farm_id",nullable = false)
+    private Farm farm;
+
+    @OneToMany(mappedBy = "field_id")
+    private List<Tree> trees;
+
+}
