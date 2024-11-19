e7523b10cbbc3023c78ba9d25519b04b217f9801
khalidwaheli <oukhakhalid@gmail.com>
2024-11-19 19:03:04 +0100

    mapper FieldMapper to map from dtos and vers versa

--- src/main/java/com/citronix/api/web/mapper/FieldMapper.java
+++ src/main/java/com/citronix/api/web/mapper/FieldMapper.java
@@ -1,14 +1,20 @@
 package com.citronix.api.web.mapper;
 
 
+import com.citronix.api.domain.Farm;
 import com.citronix.api.domain.Field;
-import com.citronix.api.web.DTO.farm.FarmCreateDTO;
+import com.citronix.api.web.DTO.field.FieldCreateDTO;
 import com.citronix.api.web.VM.ResponseFieldVM;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface FieldMapper {
-    Field toFarm(FarmCreateDTO farmCreateDTO);
+    Field toField(FieldCreateDTO fieldCreateDTO);
 
-    ResponseFieldVM toResponseFieldVM(Field field);
+    @Mapping(source = "field.id", target = "id")
+    @Mapping(source = "field.name", target = "name")
+    @Mapping(source = "field.area", target = "area")
+    @Mapping(source = "farm", target = "farm")
+    ResponseFieldVM toResponseFieldVM(Field field, Farm farm);
 }
