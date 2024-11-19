3d763ae7059a6117a3e94b87a56e3cd7f1d236a3
khalidwaheli <oukhakhalid@gmail.com>
2024-11-19 19:03:36 +0100

    field repository methods to validate area

--- src/main/java/com/citronix/api/repository/FieldRepository.java
+++ src/main/java/com/citronix/api/repository/FieldRepository.java
@@ -1,7 +1,13 @@
 package com.citronix.api.repository;
 
+import com.citronix.api.domain.Farm;
 import com.citronix.api.domain.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FieldRepository extends JpaRepository<Field, Long> {
+    Long countByFarm(Farm farm);
+
+    @Query("SELECT SUM(f.area) FROM Field f WHERE f.farm = :farm")
+    double sumAreaByFarm(Farm farm);
 }
