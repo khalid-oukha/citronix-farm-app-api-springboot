a8911d18fdacfd0ff6b3e38859034127b53d3ce8
khalidwaheli <oukhakhalid@gmail.com>
2024-11-20 20:55:36 +0100

    refactor the create dto removed status and added other status to enum

--- src/main/java/com/citronix/api/DTO/tree/TreeCreateDto.java
+++ src/main/java/com/citronix/api/DTO/tree/TreeCreateDto.java
@@ -1,6 +1,5 @@
 package com.citronix.api.DTO.tree;
 
-import com.citronix.api.domain.enums.TreeStatus;
 import com.citronix.api.utils.validation.plantingPeriod.ValidPlantingPeriod;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
@@ -19,9 +18,6 @@ public class TreeCreateDto {
     @NotNull(message = "plantationDate is required")
     @PastOrPresent(message = "must be in past or present")
     private LocalDateTime plantationDate;
-
-    @NotNull(message = "status is required")
-    private TreeStatus status;
 
     @NotNull(message = "fieldId is required")
     private Long fieldId;
