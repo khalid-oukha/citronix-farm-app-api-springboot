8f418c09991124e048049323f7f79dc854bdae56
khalidwaheli <oukhakhalid@gmail.com>
2024-11-21 22:52:15 +0100

    Updated HarvestService to use TreeRepository for checking existing harvests by field and season

---
+++ src/main/java/com/citronix/api/service/impl/HarvestDetailServiceImpl.java
@@ -0,0 +1,19 @@
+package com.citronix.api.service.impl;
+
+import com.citronix.api.domain.HarvestDetail;
+import com.citronix.api.repository.HarvestDetailsRepository;
+import com.citronix.api.service.HarvestDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class HarvestDetailServiceImpl implements HarvestDetailService {
+
+    private final HarvestDetailsRepository harvestDetailRepository;
+
+    @Override
+    public HarvestDetail create(HarvestDetail harvestDetail) {
+        return harvestDetailRepository.save(harvestDetail);
+    }
+}
---
+++ src/main/java/com/citronix/api/web/rest/HarvestController.java
@@ -0,0 +1,31 @@
+package com.citronix.api.web.rest;
+
+import com.citronix.api.DTO.harvest.HarvestCreateDTO;
+import com.citronix.api.domain.Harvest;
+import com.citronix.api.service.HarvestService;
+import com.citronix.api.web.VM.Harvest.HarvestResponseVM;
+import com.citronix.api.web.mapper.HarvestMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+public class HarvestController {
+
+    private final HarvestService harvestService;
+    private final HarvestMapper harvestMapper;
+
+    @PostMapping("/harvest")
+    public ResponseEntity<HarvestResponseVM> create(@Valid @RequestBody HarvestCreateDTO harvestCreateDTO) {
+        Harvest harvest = harvestService.create(harvestCreateDTO);
+        HarvestResponseVM response = harvestMapper.toResponse(harvest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
