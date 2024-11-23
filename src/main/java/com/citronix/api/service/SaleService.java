160bab8d75b666d018e63479af5f47d779ebf6dd
khalidwaheli <oukhakhalid@gmail.com>
2024-11-23 15:22:31 +0100

    CIT-47: As a developer, I want to implement the ability to delete a Sale

--- src/main/java/com/citronix/api/service/SaleService.java
+++ src/main/java/com/citronix/api/service/SaleService.java
@@ -1,8 +1,9 @@
 package com.citronix.api.service;
 
 import com.citronix.api.DTO.sale.SaleCreateDto;
-import com.citronix.api.domain.Harvest;
 import com.citronix.api.domain.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -11,5 +12,13 @@ public interface SaleService {
 
     Sale create(SaleCreateDto saleCreateDto);
 
-    List<Sale> findAllByHarvest(Harvest harvest);
+    void delete(Long id);
+
+    Page<Sale> getAllSales(Pageable pageable);
+
+//    List<Sale> findAllByHarvest(Harvest harvest);
+
+    Page<Sale> findAll(Pageable pageable);
+
+    List<Sale> findAllByHarvest(Long id);
 }
--- src/main/java/com/citronix/api/web/rest/SaleController.java
+++ src/main/java/com/citronix/api/web/rest/SaleController.java
@@ -7,9 +7,14 @@ import com.citronix.api.web.VM.Sale.ResponseSaleVM;
 import com.citronix.api.web.mapper.SaleMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -30,6 +35,27 @@ public class SaleController {
     public ResponseEntity<ResponseSaleVM> findById(@PathVariable Long id) {
         Sale sale = saleService.findById(id);
         ResponseSaleVM response = saleMapper.toResponse(sale);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @DeleteMapping("/sales/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        saleService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/sales/harvest")
+    public ResponseEntity<List<ResponseSaleVM>> getSalesByHarvest(@RequestParam Long harvestId) {
+        List<Sale> sales = saleService.findAllByHarvest(harvestId);
+        List<ResponseSaleVM> response = sales.stream().map(saleMapper::toResponse).toList();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/sales")
+    public ResponseEntity<List<ResponseSaleVM>> findAll(@RequestParam(defaultValue = "0") int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        Page<Sale> sales = saleService.findAll(pageable);
+        List<ResponseSaleVM> response = sales.stream().map(saleMapper::toResponse).toList();
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
